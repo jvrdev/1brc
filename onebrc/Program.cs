a9f609c16019186d956b124a034f548fd45d0b3e
Javier de Vega Ruiz <javier.devega.ruiz@gmail.com>
2024-03-16 11:06:45 +0100

    Try unsafe

--- onebrc/Program.cs
+++ onebrc/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Globalization;
 using System.IO.MemoryMappedFiles;
@@ -51,186 +52,28 @@ public class CityMeasurements
     }
 }
 
-public class ByteArrayComparer : IEqualityComparer<byte[]>
-{
-    public bool Equals(byte[]? x, byte[]? y)
-        => x.SequenceEqual(y);
-
-    public int GetHashCode(byte[] x)
-        => x.Aggregate(0, HashCode.Combine);
-}
-
 public class CalculateMetrics
 {
-
     public static readonly byte[] eolBytes = Encoding.UTF8.GetBytes(Environment.NewLine);
     public static readonly byte[] separatorBytes = ";"u8.ToArray();
 
-    //public static Task MmfBytes(string filePath)
-    //{
-    //    var bytes = new byte[512 * 1024];
-    //    var eolBytes = Encoding.UTF8.GetBytes(Environment.NewLine);
-    //    using var file = MemoryMappedFile.CreateFromFile(filePath, FileMode.Open);
-    //    using var accessor = file.CreateViewAccessor();
-    //    var capacity = accessor.Capacity;
-    //    var offset = 3L;
-    //    var dst = new ConcurrentDictionary<byte[], CityMeasurements>(new ByteArrayComparer());
-    //    while (capacity - offset > 0)
-    //    {
-    //        var readBytes = accessor.ReadArray(offset, bytes, 0, (int)Math.Min(bytes.LongLength, capacity - offset));
-    //        var readUpToEol = new ReadOnlySpan<byte>(bytes, 0, readBytes).LastIndexOf(eolBytes);
-    //        if (readUpToEol > 0)
-    //        {
-    //            offset += readUpToEol + eolBytes.Length;
-    //            CalcBytes(bytes, readUpToEol + eolBytes.Length, dst);
-    //        }
-    //        else
-    //        {
-    //            break;
-    //        }
-    //    };
+    public static readonly int cpuCount = Environment.ProcessorCount;
+    public const int pageSize = 1024 * 1024;
 
-    //    PrintResults(dst);
-
-    //    return Task.CompletedTask;
-    //}
-
-    public static Task MmfString(string filePath)
-    {
-        var bytes = new byte[1024 * 1024];
-        using var file = MemoryMappedFile.CreateFromFile(filePath, FileMode.Open);
-        using var accessor = file.CreateViewAccessor();
-        var capacity = accessor.Capacity;
-        var offset = 3L;
-        var dst = new ConcurrentDictionary<string, CityMeasurements>();
-        while (capacity - offset > 0)
-        {
-            var readBytes = accessor.ReadArray(offset, bytes, 0, (int)Math.Min(bytes.LongLength, capacity - offset));
-            var readUpToEol = new ReadOnlySpan<byte>(bytes, 0, readBytes).LastIndexOf(eolBytes);
-            if (readUpToEol > 0)
-            {
-                offset += readUpToEol + eolBytes.Length;
-                CalcString(bytes, readUpToEol + eolBytes.Length, dst);
-            }
-            else
-            {
-                break;
-            }
-        };
-
-        PrintResultsString(dst);
-
-        return Task.CompletedTask;
-    }
-
-    public readonly record struct Page(byte[] Bytes, int Length);
     public readonly record struct Page2(long Start, int Length);
 
-    public static async Task MmfStringProducerConsumer(string filePath)
-    {
-        var cpuCount = Environment.ProcessorCount;
-        using var queue = new BlockingCollection<Page>(Environment.ProcessorCount);
-        
-        var producer = Task.Run(() =>
-        {
-            using var file = MemoryMappedFile.CreateFromFile(filePath, FileMode.Open);
-            using var accessor = file.CreateViewAccessor();
-            var capacity = accessor.Capacity;
-            var offset = 3L;
-
-            while (capacity - offset > 0)
-            {
-                var bytes = new byte[1024 * 1024];
-                var readBytes = accessor.ReadArray(offset, bytes, 0, (int)Math.Min(bytes.LongLength, capacity - offset));
-                var readUpToEol = new ReadOnlySpan<byte>(bytes, 0, readBytes).LastIndexOf(eolBytes);
-                if (readUpToEol > 0)
-                {
-                    offset += readUpToEol + eolBytes.Length;
-                    queue.Add(new Page(bytes, readUpToEol + eolBytes.Length));
-
-                }
-                else
-                {
-                    break;
-                }
-            };
-
-            queue.CompleteAdding();
-        });
-
-        var consumers = Enumerable.Range(0, 10)
-            .Select(_ =>
-                Task.Run(() =>
-                {
-                    var dst = new ConcurrentDictionary<string, CityMeasurements>();
-
-                    foreach (var page in queue.GetConsumingEnumerable())
-                    { 
-                        CalcString(page.Bytes, page.Length, dst);
-                    }
-
-                    PrintResultsString(dst);
-                }));
-
-        await Task.WhenAll(consumers.Append(producer));
-    }
-    
     public static async Task MmfStringProducerConsumer2(string filePath)
     {
-        var cpuCount = Environment.ProcessorCount;
-        const int pageSize = 1024 * 1024;
         using var queue = new BlockingCollection<Page2>(cpuCount);
         using var file = MemoryMappedFile.CreateFromFile(filePath, FileMode.Open);
         using var accessor = file.CreateViewAccessor();
         
-        var producer = Task.Run(() =>
-        {
-            var capacity = accessor.Capacity;
-            var offset = 3L;
-
-            var eolWindow = new byte[eolBytes.Length];
-            while (capacity - offset > 0)
-            {
-                var count = (int)Math.Min(pageSize, capacity - offset);
-                var readUpToEol = offset + count - eolWindow.Length;
-                for (; readUpToEol >= offset; readUpToEol--)
-                {
-                    accessor.ReadArray(readUpToEol, eolWindow, 0, eolWindow.Length);
-                    if (eolWindow.SequenceEqual(eolBytes))
-                    {
-                        break;
-                    }
-                }
-                if (readUpToEol > offset)
-                {
-                    queue.Add(new Page2(offset, (int)(readUpToEol - offset) + eolBytes.Length));
-                    offset = readUpToEol + eolBytes.Length;
-                }
-                else
-                {
-                    break;
-                }
-            };
-
-            queue.CompleteAdding();
-        });
+        var producer = Task.Run(() => ProducePages(queue, accessor));
 
         var consumers = Enumerable.Range(0, cpuCount)
-            .Select(_ =>
-                Task.Run(() =>
-                {
-                    var dst = new Dictionary<string, CityMeasurements>();
-                    var bytes = new byte[pageSize];
-
-                    foreach (var page in queue.GetConsumingEnumerable())
-                    { 
-                        var readBytes = accessor.ReadArray(page.Start, bytes, 0, page.Length);
-                        CalcString(bytes, page.Length, dst);
-                    }
-
-                    return dst;
-                }))
+            .Select(_ => Task.Run(() => ConsumePages(queue, accessor)))
             .ToList();
+
         await producer;
         var dsts = await Task.WhenAll(consumers);
         var dst = dsts
@@ -241,36 +84,64 @@ public class CalculateMetrics
         PrintResultsString(dst);
     }
 
-    private static void CalcBytes(byte[] bytes, int length, IDictionary<byte[], CityMeasurements> table)
+    private static unsafe Dictionary<string, CityMeasurements> ConsumePages(BlockingCollection<Page2> queue, MemoryMappedViewAccessor accessor)
     {
-        var left = 0;
-        do
+        var dst = new Dictionary<string, CityMeasurements>();
+        var bytes = new byte[pageSize];
+
+        byte* pointer = (byte*)0;
+        accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref pointer);
+        foreach (var page in queue.GetConsumingEnumerable())
         {
-            var buffer = new ReadOnlySpan<byte>(bytes, left, length - left);
-            var indexOfSeparator = buffer.IndexOf(separatorBytes);
-            var city = buffer[0..indexOfSeparator].ToArray();
-            var indexOfEol = buffer[(indexOfSeparator + 1)..].IndexOf(eolBytes);
-            //var measurementString = Encoding.UTF8.GetString(buffer[(indexOfSeparator + 1)..(indexOfSeparator + indexOfEol + 1)]);
-            _ = csFastFloat.FastFloatParser.TryParseFloat(buffer[(indexOfSeparator + 1)..(indexOfSeparator + indexOfEol + 1)], out var measurement, styles: NumberStyles.AllowDecimalPoint);
-            if (table.TryGetValue(city, out var existingMeasurements))
+            //var readBytes = accessor.ReadArray(page.Start, bytes, 0, page.Length);
+            CalcString(new ReadOnlySpan<byte>(pointer, page.Length), dst);
+        }
+        accessor.SafeMemoryMappedViewHandle.ReleasePointer();
+
+        return dst;
+    }
+
+    private static unsafe void ProducePages(BlockingCollection<Page2> queue, MemoryMappedViewAccessor accessor)
+    {
+        var capacity = accessor.Capacity;
+        var offset = 3L;
+        byte* pointer = (byte*)0;
+        accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref pointer);
+
+        while (capacity - offset > 0)
+        {
+            var count = (int)Math.Min(pageSize, capacity - offset);
+            var readUpToEol = offset + count - eolBytes.Length;
+            for (; readUpToEol >= offset; readUpToEol--)
             {
-                existingMeasurements.AddMeasurement(measurement);
+                var eolWindow = new ReadOnlySpan<byte>(pointer + readUpToEol, eolBytes.Length);
+                //accessor.ReadArray(readUpToEol, eolWindow, 0, eolWindow.Length);
+                if (eolWindow.SequenceEqual(eolBytes))
+                {
+                    break;
+                }
+            }
+            if (readUpToEol > offset)
+            {
+                queue.Add(new Page2(offset, (int)(readUpToEol - offset) + eolBytes.Length));
+                offset = readUpToEol + eolBytes.Length;
             }
             else
             {
-                table[city] = new CityMeasurements(measurement);
+                break;
             }
-            left += indexOfSeparator + indexOfEol + eolBytes.Length + 1;
-        }
-        while (left < length);
+        };
+        accessor.SafeMemoryMappedViewHandle.ReleasePointer();
+
+        queue.CompleteAdding();
     }
 
-    private static void CalcString(byte[] bytes, int length, IDictionary<string, CityMeasurements> table)
+    private static void CalcString(ReadOnlySpan<byte> page, IDictionary<string, CityMeasurements> table)
     {
         var left = 0;
         do
         {
-            var buffer = new ReadOnlySpan<byte>(bytes, left, length - left);
+            var buffer = page[left..page.Length];
             var indexOfSeparator = buffer.IndexOf(separatorBytes);
             var city = Encoding.UTF8.GetString(buffer[0..indexOfSeparator]);
             var indexOfEol = buffer[(indexOfSeparator + 1)..].IndexOf(eolBytes);
@@ -286,15 +157,8 @@ public class CalculateMetrics
             }
             left += indexOfSeparator + indexOfEol + eolBytes.Length + 1;
         }
-        while (left < length);
+        while (left < page.Length);
     }
-
-    //public static void PrintResults<T>(IDictionary<T, CityMeasurements> table)
-    //{
-    //    Console.Write("{");
-    //    Console.Write(string.Join(", ", table.OrderBy(x => x.Key).Select(x => x.Value.Summary(x.Key))));
-    //    Console.WriteLine("}");
-    //}
 
     public static void PrintResultsString(IDictionary<string, CityMeasurements> table)
     {
@@ -311,9 +175,6 @@ public class Program
     {
         var filePath = args[0];
 
-        //await CalculateMetrics<ReadOnlyMemory<char>>.PrintCalculatedMetricsString(filePath);
-        //await CalculateMetrics<ReadOnlyMemory<char>>.BlockingCollection(filePath);
-        //await CalculateMetrics.MmfBytes(filePath);
         await CalculateMetrics.MmfStringProducerConsumer2(filePath);
 
         return 0;
